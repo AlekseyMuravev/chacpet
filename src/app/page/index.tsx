b62581621b6347d2a10c5515dd52fa278a6ca15d
Aleksey <muravev.aleksey2@wb.ru>
2023-07-07 22:47:49 +0300

    upd package

--- src/app/page/index.tsx
+++ src/app/page/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.scss';
 export const MainPage = () => {
   return (
     <div className={styles.wrapper}>
-      Это тестовая главня страница
+      123
     </div>
   );
 };
